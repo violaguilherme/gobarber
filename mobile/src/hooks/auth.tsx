c355a97e2622b41fd2860cbbe608cccac024caf1
violaguilherme <guilherme_schott@hotmail.com>
2023-03-18 14:45:04 -0300

    Finish SignIn and SignUp pages and add routes

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -16,6 +16,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
     user: object
+    loading: boolean
     signIn(credentials: SignInCredentials): Promise<void>
     signOut(): void
 }
@@ -24,6 +25,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
 const AuthProvider: React.FC = ({ children }) => {
     const [data, setData] = useState<AuthState>({} as AuthState)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         async function loadStorageData(): Promise<void> {
@@ -32,8 +34,9 @@ const AuthProvider: React.FC = ({ children }) => {
             if (token[1] && user[1]) {
                 setData({ token: token[1], user: JSON.parse(user[1]) })
             }
-        }
 
+            setLoading(false)
+        }
 
         loadStorageData()
     }, [])
@@ -60,7 +63,7 @@ const AuthProvider: React.FC = ({ children }) => {
     }, [])
 
     return (
-        <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+        <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
             { children }
         </AuthContext.Provider>
     )
