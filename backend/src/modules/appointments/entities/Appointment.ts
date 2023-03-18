b4149e5de96194b4ae311d21e29c951600206ef4
violaguilherme <guilherme_schott@hotmail.com>
2023-03-18 15:38:16 -0300

    Start reorganizing backend folders structure

--- backend/src/modules/appointments/entities/Appointment.ts
+++ backend/src/modules/appointments/entities/Appointment.ts
@@ -1,6 +1,6 @@
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 
-import User from "./User"
+import User from "../../users/entities/User"
 
 @Entity("appointments")
 class Appointment {
