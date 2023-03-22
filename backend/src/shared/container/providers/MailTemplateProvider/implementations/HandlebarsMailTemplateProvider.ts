86413cb77b67e32ad71337eba6a551d5fae8d874
violaguilherme <guilherme_schott@hotmail.com>
2023-03-22 14:42:16 -0300

    Add MailTemplateProvider and handlebars to send html in the email

---
+++ backend/src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -0,0 +1,14 @@
+import Handlebars from "handlebars";
+
+import IMailTemplateProvider from "../models/IMailTemplateProvider";
+import IParseMailTemplateDTO from "../dtos/IParseMailTemplateDTO";
+
+class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
+    public async parse({ template, variables }: IParseMailTemplateDTO): Promise<string> {
+        const parseTemplate = Handlebars.compile(template)
+
+        return parseTemplate(variables)
+    }
+}
+
+export default HandlebarsMailTemplateProvider
--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -6,6 +6,20 @@ import IStorageProvider from "./StorageProvider/models/IStorageProvider";
 import EtherealMailProvider from "./MailProvider/implementations/EtherealMailProvider";
 import IMailProvider from "./MailProvider/models/IMailProvider";
 
-container.registerSingleton<IStorageProvider>("StorageProvider", DiskStorageProvider)
+import HandlebarsMailTemplateProvider from "./MailTemplateProvider/implementations/HandlebarsMailTemplateProvider"; 
+import IMailTemplateProvider from "./MailTemplateProvider/models/IMailTemplateProvider";
 
-container.registerInstance<IMailProvider>("MailProvider", new EtherealMailProvider())
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider", 
+    DiskStorageProvider
+)
+
+container.registerSingleton<IMailTemplateProvider>(
+    "MailTemplateProvider", 
+    HandlebarsMailTemplateProvider
+)
+    
+container.registerInstance<IMailProvider>(
+    "MailProvider", 
+    container.resolve(EtherealMailProvider)
+)
