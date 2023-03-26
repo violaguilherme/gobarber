0336d181a53b24c29103a0a0a21667d69279c17f
violaguilherme <guilherme_schott@hotmail.com>
2023-03-26 14:55:05 -0300

    Add config to s3Provider

--- backend/src/modules/users/infra/http/routes/users.routes.ts
+++ backend/src/modules/users/infra/http/routes/users.routes.ts
@@ -8,9 +8,10 @@ import UsersController from "../controllers/UsersController"
 import UserAvatarController from "../controllers/UserAvatarController"
 
 const usersRouter = Router()
-const upload = multer(uploadConfig)
 const usersController = new UsersController()
 const userAvatarController = new UserAvatarController()
+
+const upload = multer(uploadConfig.multer)
 
 usersRouter.post("/", celebrate({
     [Segments.BODY]: {
