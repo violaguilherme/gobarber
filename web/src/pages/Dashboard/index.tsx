751447c1649f2a0660109e40eb44b72e9257a577
violaguilherme <guilherme_schott@hotmail.com>
2023-03-30 20:33:22 -0300

    Create schedule part in dashboard

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { FiPower } from "react-icons/fi";
+import { FiClock, FiPower } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg"
 import { useAuth } from "../../hooks/auth";
-import { Container, Header, HeaderContent, Profile } from "./styles";
+import { 
+    Container, 
+    Header, 
+    HeaderContent, 
+    Profile, 
+    Content, 
+    Schedule, 
+    Calendar, 
+    NextAppointment 
+} from "./styles";
 
 const DashBoard: React.FC = () => {
     const { signOut, user } = useAuth()
@@ -32,6 +41,35 @@ const DashBoard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Horários agendados</h1>
+                    <p>
+                        <span>Hoje</span>
+                        <span>Dia 06</span>
+                        <span>Segunda-feira</span>
+                    </p>
+
+                    <NextAppointment>
+                        <strong>Atendimento a seguir</strong>
+
+                        <div>
+                            <img 
+                                src="https://avatars.githubusercontent.com/u/107087686?v=4" 
+                                alt="Guilherme" 
+                            />
+
+                            <strong>Guilherme Viola</strong>
+                            <span><FiClock />10:00</span>
+                        </div>
+                    </NextAppointment>
+                </Schedule>
+
+                <Calendar>
+
+                </Calendar>
+            </Content>
         </Container>
     )
 }
--- web/src/pages/Dashboard/styles.ts
+++ web/src/pages/Dashboard/styles.ts
@@ -56,3 +56,94 @@ export const Profile = styled.div`
         }
     }
 `
+
+export const Content = styled.main`
+    max-width: 120rem;
+    margin: 6.4rem auto;
+    display: flex;
+`
+
+export const Schedule = styled.div`
+    flex: 1;
+    margin-right: 12rem;
+
+    h1 {
+        font-size: 3.6rem;
+    }
+
+    p {
+        display: flex;
+        margin-top: .8rem;
+        color: #ff9000;
+        font-weight: 500;
+
+        span {
+            display: flex;
+            align-items: center;
+        }
+
+        span + span::before {
+            content: "";
+            width: .1rem;
+            height: 1.6rem;
+            background: #ff9000;
+            margin: 0 .8rem;
+        }
+    }
+`
+export const NextAppointment = styled.div`
+    margin-top: 6.4rem;
+
+    > strong {
+        color: #999591;
+        font-size: 2rem;
+        font-weight: 400;
+    }
+
+    div {
+        background: #3e3b47;
+        display: flex;
+        align-items: center;
+        padding: 1.6rem 2.4rem;
+        border-radius: 1rem;
+        margin-top: 2.4rem;
+        position: relative;
+
+        &::before {
+            content: "";
+            position: absolute;
+            height: 80%;
+            width: .1rem;
+            left: 0;
+            top: 10%;
+            background: #ff9000;
+        }
+
+        img {
+            width: 8rem;
+            height: 8rem;
+            border-radius: 50%;
+        }
+
+        strong {
+            margin-left: 2.4rem;
+            color: #fff;
+        }
+
+        span {
+            margin-left: auto;
+            display: flex;
+            align-items: center;
+            color: #999591;
+
+            svg {
+                color: #ff9000;
+                margin-right: .8rem;
+            }
+        }
+    }
+`
+
+export const Calendar = styled.aside`
+    width: 38rem;
+`
